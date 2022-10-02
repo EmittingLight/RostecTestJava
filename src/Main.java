33a3b7beb5556b50e3239b1e6a09c1ae72fe0af2
SergeiPetrov1974 <58001369+SergeiPetrov1974@users.noreply.github.com>
2022-10-03 00:12:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] array = {1, 3, 4, 5, 1, 5, 4};
+        System.out.print(Arrays.toString(array) + " -> " + numberIdenticalValues(array));
+
+
+    }
+
+    public static Map<Integer, Integer> numberIdenticalValues(Integer[] array) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer integer : array) {
+            if (map.containsKey(integer)) {
+                map.put(integer, map.get(integer) + 1);
+            } else {
+                map.put(integer, 1);
+            }
+        }
+        return map;
+    }
+}
